3f0778ca9f9c3f8c9ed6d5f5298fa88a54e1a2f1
Santi Cano <canosantiago404@gmail.com>
2021-09-17 12:03:44 -0300

    added operate function

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-const add = function(a, b) {
+function add(a, b) {
     return a + b;
 }
 
-const subtract = function(a, b) {
+function substract(a, b) {
     return a - b;
 }
 
-const multiply = function(a, b) {
+function multiply(a, b) {
     return a * b;
 }
 
-const divide = function(a, b) {
+function divide(a, b) {
     return a / b;
+}
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return substract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b === 0) return a;
+            return divide(a, b);
+        default:
+            return null;
+    } 
+
 }
